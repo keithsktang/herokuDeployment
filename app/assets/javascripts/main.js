02d1ee13ee490dcd7040fbb9a8bd8e163b7ff059
Keith Tang <keithsktang@gmail.com>
2018-02-09 22:43:55 -0500

    addangular

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,42 @@
+var app = angular.module('main', []);
+app.controller('deployment', function($scope, $http, $timeout) {
+  $scope.name = "name"
+
+  $http.get("https://api.heroku.com/apps/pure-beach-57030/builds", {
+    headers: {
+      'Accept': 'application/vnd.heroku+json; version=3',
+      "Content-Type": "application/json",
+      "Authorization": "Bearer e57a8130-c699-4c2b-b152-4b52be8badc7"
+    }
+  }).then(function(response) {
+    $scope.herokuCommit = response.data[response.data.length - 1].source_blob.version;
+  })
+  $http.get("https://api.github.com/repos/keithsktang/herokuDeployment/commits", {
+    headers: {
+      'Accept': 'application/vnd.github.v3+json',
+      "Content-Type": "application/json",
+      'User-Agent': 'keithsktang'
+    }
+  }).then(function(response) {
+    $scope.githubCommit = response.data[0].sha;
+  })
+  $timeout(function(){
+    $scope.githubCommit === $scope.herokuCommit ? null : $scope.newVersion
+  },0)
+  $scope.deploy = function(version){
+    $http.get(`https://vne365avwi.execute-api.us-east-1.amazonaws.com/api/${version}`)
+  }
+
+
+  // @heroku_res = HTTParty.get("https://api.heroku.com/apps/pure-beach-57030/builds",
+  //     :headers => {'Accept' => 'application/vnd.heroku+json; version=3',
+  //     "Content-Type" => "application/json",
+  //             "Authorization" => "Bearer e57a8130-c699-4c2b-b152-4b52be8badc7"})
+  // @heroku_commit = @heroku_res.last['source_blob']['version']
+  // @git_res = HTTParty.get("https://api.github.com/repos/keithsktang/herokuDeployment/commits",
+  //     :headers => {'Accept' => 'application/vnd.github.jean-grey-preview+json',
+  //                   'User-Agent' => 'keithsktang'})
+  // @git_commit = @git_res.first['sha']
+  //
+  // @new_version if @git_commit != @heroku_commit
+})
