b7ac3bec47773f200d93806ab8476fc557f38769
Keith Tang <keithsktang@gmail.com>
2018-02-10 21:02:07 -0500

    add key

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,27 +1,33 @@
 var app = angular.module('main', []);
 app.controller('deployment', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
-  $scope.name = "name"
-  $http.get("https://api.heroku.com/apps/pure-beach-57030/builds", {
-    headers: {
-      'Accept': 'application/vnd.heroku+json; version=3',
-      "Content-Type": "application/json",
-      "Authorization": process.env.HEROKU_KEY
-    }
-  }).then(function(response) {
-    $scope.herokuCommit = response.data[response.data.length - 1].source_blob.version;
-  }).finally(function(){
-    $http.get("https://api.github.com/repos/keithsktang/herokuDeployment/commits", {
+  $scope.init = function() {
+    $scope.key = document.getElementById('key').innerText;
+    console.log($scope.key)
+  }
+  $timeout(function() {
+    $http.get("https://api.heroku.com/apps/pure-beach-57030/builds", {
       headers: {
-        'Accept': 'application/vnd.github.v3+json',
-        "Content-Type": "application/json"
-        // 'User-Agent': 'keithsktang'
+        'Accept': 'application/vnd.heroku+json; version=3',
+        "Content-Type": "application/json",
+        "Authorization": $scope.key
       }
     }).then(function(response) {
-      $scope.githubCommit = response.data[0].sha;
-    }).finally(function(){
-      $scope.githubCommit === $scope.herokuCommit ? $scope.newVersion = false : $scope.newVersion = true
+      $scope.herokuCommit = response.data[response.data.length - 1].source_blob.version;
+    }).finally(function() {
+      $http.get("https://api.github.com/repos/keithsktang/herokuDeployment/commits", {
+        headers: {
+          'Accept': 'application/vnd.github.v3+json',
+          "Content-Type": "application/json"
+          // 'User-Agent': 'keithsktang'
+        }
+      }).then(function(response) {
+        $scope.githubCommit = response.data[0].sha;
+      }).finally(function() {
+        $scope.githubCommit === $scope.herokuCommit ? $scope.newVersion = false : $scope.newVersion = true
+      })
     })
-  })
+  }, 10);
+
   // $scope.deploy = function(version){
   //   $http.get(`https://vne365avwi.execute-api.us-east-1.amazonaws.com/api/${version}`).then(function(response){
   //     $scope.awsResponse = response;
